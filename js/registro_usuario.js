8257ea7ccfae425860619f5e04d5108253475033
Fabithelast25 <fabian.robles06@inacapmail.cl>
2025-04-28 00:00:25 -0400

    Subida de JS de la página de registro de usuario

---
+++ js/registro_usuario.js
@@ -0,0 +1,6 @@
+let boton_registrar = document.getElementById("button_register")
+
+boton_registrar.addEventListener('click', function() { 
+    alert("¡Te has registrado con éxito!"); 
+    open("login.html")
+});
