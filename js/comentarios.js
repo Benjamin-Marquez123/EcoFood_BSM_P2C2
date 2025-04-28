e63e20c3e9b1dbad47adc03f74581f0b104f9724
Benjamin-Marquez123 <benjamin.silva39@inacapmail.cl>
2025-04-27 21:42:55 -0400

    JS de la pagina comentarios

---
+++ js/comentarios.js
@@ -0,0 +1,41 @@
+// Espera a que la pagina cargue completamente y luego ejecuta el codigo dentro
+document.addEventListener('DOMContentLoaded', function() {
+
+    //Guardar el formulario en una variable y el div comentarios en otra
+    const formulario = document.getElementById('formulario');
+    const Comentarios = document.getElementById('comentarios');
+
+    //Cuando se envie el formulario comienza el codigo
+    formulario.addEventListener('submit', function(enviar) {
+        // Evita que el formulario recargue la página
+        enviar.preventDefault(); 
+
+        // Obtener los valores del formulario y quitarle espacios por si acaso
+        const nombre = document.getElementById('nombre').value.trim();
+        const correo = document.getElementById('correo').value.trim();
+        const comentario = document.getElementById('comentario').value.trim();
+
+        // Se crea el comentario en formato "card" 
+        const nuevoComentario = document.createElement('div');
+        nuevoComentario.classList.add('card');
+        nuevoComentario.innerHTML = `
+            <div class="card-body">
+                <h5 class="card-title">
+                    ${nombre}
+                </h5>
+                <h6 class="card-subtitle">
+                    (${correo})
+                </h6>
+                <p class="card-text">
+                    ${comentario}
+                </p>
+            </div>
+        `;
+
+        // Insertar el nuevo comentario **al principio** del contenedor
+        Comentarios.prepend(nuevoComentario);
+
+        // Limpiar los campos del formulario
+        formulario.reset();
+    });
+});
